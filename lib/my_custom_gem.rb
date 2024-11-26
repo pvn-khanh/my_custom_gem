060d281e84b5616915cdd10591724ff9be00a58f
KhanhLT <khanh.l@peraichi.co.jp>
2024-11-26 21:34:57 +0700

    init: create a custom gem

---
+++ lib/my_custom_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "my_custom_gem/version"
+
+module MyCustomGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/my_custom_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MyCustomGem
+  VERSION = "0.1.0"
+end
