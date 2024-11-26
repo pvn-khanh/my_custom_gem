ccaa416badb9f004f1b101ac69179d756182379c
KhanhLT <khanh.l@peraichi.co.jp>
2024-11-27 01:48:41 +0700

    ver 0.11.0

--- lib/my_custom_gem.rb
+++ lib/my_custom_gem.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-require_relative "my_custom_gem/version"
-# require "my_custom_gem/greeter"
+require "my_custom_gem/version"
+require "my_custom_gem/greeter"
 
 module MyCustomGem
   class Error < StandardError; end
 
   def self.hello
-    puts 123
-    # Greeter.greet("world, version: " + self::VERSION)
+    Greeter.greet("world, version: " + self::VERSION)
   end
 end
--- lib/my_custom_gem/version.rb
+++ lib/my_custom_gem/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module MyCustomGem
-  VERSION = "0.9.0"
+  VERSION = "0.11.0"
 end
